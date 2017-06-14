d91e520a1572a6675973cc709cd2ceef15c4e1eb
prjseal <prjseal@gmail.com>
2017-06-14 17:12:06 +0100

    - changed validator to sut to follow TDD naming convention

--- NINumberValidator.Test/NIValidatorTests.cs
+++ NINumberValidator.Test/NIValidatorTests.cs
@@ -9,40 +9,40 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_A_String_Longer_Than_9_Characters_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456AB");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456AB");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_A_String_Less_Than_9_Characters_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_An_Empty_String_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_A_Null_Value_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid(null);
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid(null);
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_A_String_Which_is_9_Characters_Long_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456A");
             Assert.IsTrue(result);
         }
 
@@ -50,32 +50,32 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_First_2_Characters_Are_Numeric_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("00123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("00123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_Numeric_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("0J123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("0J123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_Numeric_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("J0123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("J0123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_Alpha_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456A");
             Assert.IsTrue(result);
         }
 
@@ -83,24 +83,24 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_Next_6_Characters_Are_Numeric_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456A");
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void Given_First_Of_Next_6_Characters_Are_Not_Numeric_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJJ23456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJJ23456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_All_Next_6_Characters_Are_Not_Numeric_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJJJJJJJA");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJJJJJJJA");
             Assert.IsFalse(result);
         }
 
@@ -108,48 +108,48 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_Final_Character_Is_A_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456A");
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void Given_Final_Character_Is_B_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456B");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456B");
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void Given_Final_Character_Is_C_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456C");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456C");
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void Given_Final_Character_Is_D_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456D");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456D");
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void Given_Final_Character_Is_E_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456E");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456E");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Final_Character_Is_Z_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JJ123456Z");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JJ123456Z");
             Assert.IsFalse(result);
         }
 
@@ -157,48 +157,48 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_First_Character_Is_D_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("DJ123456D");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("DJ123456D");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_F_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("FJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("FJ123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_I_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("IJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("IJ123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_Q_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("QJ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("QJ123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_U_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("UJ123456C");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("UJ123456C");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Character_Is_V_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("VJ123456C");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("VJ123456C");
             Assert.IsFalse(result);
         }
 
@@ -206,56 +206,56 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_Second_Character_Is_D_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JD123456D");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JD123456D");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_F_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JF123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JF123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_I_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JI123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JI123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_O_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JO123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JO123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_Q_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JQ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JQ123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_U_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JU123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JU123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_Second_Character_Is_V_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("JV123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("JV123456A");
             Assert.IsFalse(result);
         }
 
@@ -263,64 +263,64 @@ namespace NINumberValidator.Test
         [TestMethod]
         public void Given_First_Two_Characters_Are_GB_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("GB123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("GB123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_BG_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("BG123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("BG123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_NK_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("NK123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("NK123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_KN_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("KN123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("KN123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_TN_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("TN123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("TN123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_NT_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("NT123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("NT123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_ZZ_Should_Return_False()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("ZZ123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("ZZ123456A");
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void Given_First_Two_Characters_Are_Allowed_Should_Return_True()
         {
-            NIValidator validator = new NIValidator();
-            bool result = validator.IsValid("HG123456A");
+            NIValidator sut = new NIValidator();
+            bool result = sut.IsValid("HG123456A");
             Assert.IsTrue(result);
         }
     }
